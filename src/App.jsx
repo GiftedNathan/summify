41fda53fb7b8cb7d8213935d7e6e6f5c36d8957f
giftedNathan <eje.nathaniel@gmail.com>
2023-09-08 15:08:55 +0100

    First commit - with header created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+
+function App() {
+
+  return (
+    <main className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+      <Header />
+    </main>
+  )
+}
+
+export default App
